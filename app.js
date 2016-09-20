7866eeb103ad9732cc4d91a5b63c25de5a1f0940
bongster <bongster88@gmail.com>
2016-09-21 01:39:08 +0900

    Add default template using bootstrap4

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.use(session({
 app.use(require('node-sass-middleware')({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
+  debug: true,
   indentedSyntax: true,
   sourceMap: true
 }));
